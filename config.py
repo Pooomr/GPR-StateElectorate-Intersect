d920be50259f7eecb9f1fd6558f29dd5ca579080
Pooomr <140774543+Pooomr@users.noreply.github.com>
2024-02-09 13:45:52 +1100

    Create config.py
    Config file for database connection details

---
+++ config.py
@@ -0,0 +1,9 @@
+#config file for GPR Electorate Update
+
+# Settings for GPR Oracle Database connection PROD
+username = ''
+password = ''
+dsnDCS = '' #DCS
+dsnDPE = '' #DPE
+port = 
+encoding = 'UTF-8'
